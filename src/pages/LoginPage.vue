5bed0e25356c9338756702fc9299307562ccec70
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2023-11-10 16:27:53 +0600

    Update LoginPage.vue

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,15 +1,12 @@
 <template>
-  <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+  <div class="mx-auto max-w-screen-xl grid h-screen place-content-center px-4 py-16 sm:px-6 lg:px-8 w-1/2">
     <div class="mx-auto max-w-lg text-center">
       <h1 class="text-2xl font-bold sm:text-3xl">Get started today!</h1>
-
-      <p class="mt-4 text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla
-        eaque error neque ipsa culpa autem, at itaque nostrum!
-      </p>
+      <a class="block text-teal-600" href="/">
+        <span class="">-- Home --</span>
+      </a>
     </div>
-
-    <form action="" class="mx-auto mb-0 mt-8 max-w-md space-y-4">
+    <form action="" class="mx-auto mb-0 mt-8 max-w-md space-y-4 ">
       <div>
         <label for="email" class="sr-only">Email</label>
 
@@ -77,7 +74,7 @@
       <div class="flex items-center justify-between">
         <p class="text-sm text-gray-500">
           No account?
-          <a class="underline" href="">Sign up</a>
+          <a class="underline" href="/register">Sign up</a>
         </p>
 
         <button
@@ -86,6 +83,7 @@
         >
           Sign in
         </button>
+
       </div>
     </form>
   </div>
