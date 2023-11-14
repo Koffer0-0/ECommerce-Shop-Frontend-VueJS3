21ca0aa8d653526bb68f2f9f429f3eab2eda530b
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2023-11-14 18:23:57 +0600

    make the page dynamic and possible get data

--- src/components/DetailsList.vue
+++ src/components/DetailsList.vue
@@ -5,40 +5,31 @@
       <div
           class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4"
       >
-        <dt class="font-medium text-gray-900">Title</dt>
-        <dd class="text-gray-700 sm:col-span-2">Mr</dd>
+        <dt class="font-medium text-gray-900">Order ID</dt>
+        <dd class="text-gray-700 sm:col-span-2">{{ order.id }}</dd>
+      </div>
+      <div
+          class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4"
+      >
+        <dt class="font-medium text-gray-900">Date</dt>
+        <dd class="text-gray-700 sm:col-span-2">{{ order.date }}</dd>
       </div>
 
       <div
           class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4"
       >
-        <dt class="font-medium text-gray-900">Name</dt>
-        <dd class="text-gray-700 sm:col-span-2">John Frusciante</dd>
+        <dt class="font-medium text-gray-900">Total</dt>
+        <dd class="text-gray-700 sm:col-span-2">{{ order.amount }}</dd>
       </div>
 
       <div
           class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4"
       >
-        <dt class="font-medium text-gray-900">Occupation</dt>
-        <dd class="text-gray-700 sm:col-span-2">Guitarist</dd>
-      </div>
-
-      <div
-          class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4"
-      >
-        <dt class="font-medium text-gray-900">Salary</dt>
-        <dd class="text-gray-700 sm:col-span-2">$1,000,000+</dd>
-      </div>
-
-      <div
-          class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4"
-      >
-        <dt class="font-medium text-gray-900">Bio</dt>
+        <dt class="font-medium text-gray-900">Details</dt>
         <dd class="text-gray-700 sm:col-span-2">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et facilis
-          debitis explicabo doloremque impedit nesciunt dolorem facere, dolor
-          quasi veritatis quia fugit aperiam aspernatur neque molestiae labore
-          aliquam soluta architecto?
+          <p v-for="item in order.details">
+            {{item.name}} x {{item.quantity}} = {{item.price}} Tenge
+          </p>
         </dd>
       </div>
     </dl>
@@ -47,6 +38,22 @@
 
 <script setup>
 import router from "@/router";
+import {onMounted, ref} from "vue";
+import {useOrder} from "@/composables/useOrder";
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+
+const order = ref({id: 1, amount:5000, date: "12.10.2023", details:[{name:"rubashka", quantity: 1, price: 1233},{name:"futbolka", quantity: 2, price:1233}]})
+const {handleFetchOrderById} = useOrder()
+onMounted(() => {
+  const orderId = route.params.id;
+  getOrderInfo(orderId)
+})
+
+const getOrderInfo = async (id) => {
+  order.value = await handleFetchOrderById(id)
+}
 </script>
 
 <style scoped>
