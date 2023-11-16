ee3779eb92a7f971048b891d164304b278efcca8
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-11-16 16:11:07 +0600

    Update useOrder.js

--- src/composables/useOrder.js
+++ src/composables/useOrder.js
@@ -1,4 +1,4 @@
-import {getOrderById, getOrders} from "@/api/orders";
+import {createOrders, getOrderById, getOrders} from "@/api/orders";
 
 export function useOrder(){
     const handleFetchOrders = async (id) => {
@@ -19,9 +19,19 @@ export function useOrder(){
             }
         }
     }
+    const handleCheckout = async (payload) => {
+        try {
+            return await createOrders(payload)
+        } catch (err) {
+            if (process.env.NODE_ENV === 'development') {
+                console.log(err);
+            }
+        }
+    }
 
     return {
         handleFetchOrders,
-        handleFetchOrderById
+        handleFetchOrderById,
+        handleCheckout
     }
 }
