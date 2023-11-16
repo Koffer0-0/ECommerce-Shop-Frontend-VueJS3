d6832d4f0f6ab18a53fe1fe685c9d683c65d678d
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-11-16 16:11:02 +0600

    Update ProductList.vue

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -7,9 +7,9 @@
         </h2>
 
         <p class="mt-4 max-w-md text-gray-500">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
-          praesentium cumque iure dicta incidunt est ipsam, officia dolor fugit
-          natus?
+<!--          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque-->
+<!--          praesentium cumque iure dicta incidunt est ipsam, officia dolor fugit-->
+<!--          natus?-->
         </p>
       </header>
 
@@ -38,102 +38,102 @@
         </div>
 
         <div class="hidden sm:flex sm:gap-4">
-          <div class="relative">
-            <details class="group [&_summary::-webkit-details-marker]:hidden">
-              <summary
-                  class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"
-              >
-                <span class="text-sm font-medium"> Availability </span>
+<!--          <div class="relative">-->
+<!--            <details class="group [&_summary::-webkit-details-marker]:hidden">-->
+<!--              <summary-->
+<!--                  class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"-->
+<!--              >-->
+<!--                <span class="text-sm font-medium"> Availability </span>-->
 
-                <span class="transition group-open:-rotate-180">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-4 w-4"
-                >
-                  <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                  />
-                </svg>
-              </span>
-              </summary>
+<!--                <span class="transition group-open:-rotate-180">-->
+<!--                <svg-->
+<!--                    xmlns="http://www.w3.org/2000/svg"-->
+<!--                    fill="none"-->
+<!--                    viewBox="0 0 24 24"-->
+<!--                    stroke-width="1.5"-->
+<!--                    stroke="currentColor"-->
+<!--                    class="h-4 w-4"-->
+<!--                >-->
+<!--                  <path-->
+<!--                      stroke-linecap="round"-->
+<!--                      stroke-linejoin="round"-->
+<!--                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"-->
+<!--                  />-->
+<!--                </svg>-->
+<!--              </span>-->
+<!--              </summary>-->
 
-              <div
-                  class="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0"
-              >
-                <div class="w-96 rounded border border-gray-200 bg-white">
-                  <header class="flex items-center justify-between p-4">
-                    <span class="text-sm text-gray-700"> 0 Selected </span>
+<!--              <div-->
+<!--                  class="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0"-->
+<!--              >-->
+<!--                <div class="w-96 rounded border border-gray-200 bg-white">-->
+<!--                  <header class="flex items-center justify-between p-4">-->
+<!--                    <span class="text-sm text-gray-700"> 0 Selected </span>-->
 
-                    <button
-                        type="button"
-                        class="text-sm text-gray-900 underline underline-offset-4"
-                    >
-                      Reset
-                    </button>
-                  </header>
+<!--                    <button-->
+<!--                        type="button"-->
+<!--                        class="text-sm text-gray-900 underline underline-offset-4"-->
+<!--                    >-->
+<!--                      Reset-->
+<!--                    </button>-->
+<!--                  </header>-->
 
-                  <ul class="space-y-1 border-t border-gray-200 p-4">
-                    <li>
-                      <label
-                          for="FilterInStock"
-                          class="inline-flex items-center gap-2"
-                      >
-                        <input
-                            type="checkbox"
-                            id="FilterInStock"
-                            class="h-5 w-5 rounded border-gray-300"
-                        />
+<!--                  <ul class="space-y-1 border-t border-gray-200 p-4">-->
+<!--                    <li>-->
+<!--                      <label-->
+<!--                          for="FilterInStock"-->
+<!--                          class="inline-flex items-center gap-2"-->
+<!--                      >-->
+<!--                        <input-->
+<!--                            type="checkbox"-->
+<!--                            id="FilterInStock"-->
+<!--                            class="h-5 w-5 rounded border-gray-300"-->
+<!--                        />-->
 
-                        <span class="text-sm font-medium text-gray-700">
-                        In Stock (5+)
-                      </span>
-                      </label>
-                    </li>
+<!--                        <span class="text-sm font-medium text-gray-700">-->
+<!--                        In Stock (5+)-->
+<!--                      </span>-->
+<!--                      </label>-->
+<!--                    </li>-->
 
-                    <li>
-                      <label
-                          for="FilterPreOrder"
-                          class="inline-flex items-center gap-2"
-                      >
-                        <input
-                            type="checkbox"
-                            id="FilterPreOrder"
-                            class="h-5 w-5 rounded border-gray-300"
-                        />
+<!--                    <li>-->
+<!--                      <label-->
+<!--                          for="FilterPreOrder"-->
+<!--                          class="inline-flex items-center gap-2"-->
+<!--                      >-->
+<!--                        <input-->
+<!--                            type="checkbox"-->
+<!--                            id="FilterPreOrder"-->
+<!--                            class="h-5 w-5 rounded border-gray-300"-->
+<!--                        />-->
 
-                        <span class="text-sm font-medium text-gray-700">
-                        Pre Order (3+)
-                      </span>
-                      </label>
-                    </li>
+<!--                        <span class="text-sm font-medium text-gray-700">-->
+<!--                        Pre Order (3+)-->
+<!--                      </span>-->
+<!--                      </label>-->
+<!--                    </li>-->
 
-                    <li>
-                      <label
-                          for="FilterOutOfStock"
-                          class="inline-flex items-center gap-2"
-                      >
-                        <input
-                            type="checkbox"
-                            id="FilterOutOfStock"
-                            class="h-5 w-5 rounded border-gray-300"
-                        />
+<!--                    <li>-->
+<!--                      <label-->
+<!--                          for="FilterOutOfStock"-->
+<!--                          class="inline-flex items-center gap-2"-->
+<!--                      >-->
+<!--                        <input-->
+<!--                            type="checkbox"-->
+<!--                            id="FilterOutOfStock"-->
+<!--                            class="h-5 w-5 rounded border-gray-300"-->
+<!--                        />-->
 
-                        <span class="text-sm font-medium text-gray-700">
-                        Out of Stock (10+)
-                      </span>
-                      </label>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </details>
-          </div>
+<!--                        <span class="text-sm font-medium text-gray-700">-->
+<!--                        Out of Stock (10+)-->
+<!--                      </span>-->
+<!--                      </label>-->
+<!--                    </li>-->
+<!--                  </ul>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </details>-->
+<!--          </div>-->
 
           <div class="relative">
             <details class="group [&_summary::-webkit-details-marker]:hidden">
@@ -273,7 +273,6 @@ onMounted(async () => {
 
 // Add a product to the cart
 function handleAddToCart(product) {
-
   console.log(product)
   addToCart(product);
 }
