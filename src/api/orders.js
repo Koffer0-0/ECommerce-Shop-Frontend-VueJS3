b0ad51a3d72c5125ab11984f89bb88d943f8e87d
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-11-16 16:10:43 +0600

    Update orders.js

--- src/api/orders.js
+++ src/api/orders.js
@@ -1,14 +1,31 @@
 import axios from "axios";
 import {API_URL} from "@/utils/consts";
 
+const token = localStorage.getItem('access_token');
+
 export function getOrders() {
-    return axios.get(API_URL + 'orders', {
-        headers: {'Content-Type': 'application/x-www-form-urlencoded', 'accept': 'application/json'}
+    return axios.get(API_URL + 'order/all', {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'accept': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    });
+}
+export function createOrders(payload) {
+    return axios.post(API_URL + 'order/create', payload,{
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
     });
 }
 
 export function getOrderById(id) {
     return axios.get(API_URL + `order/${id}`, {
-        headers: {'Content-Type': 'application/x-www-form-urlencoded', 'accept': 'application/json'}
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'accept': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
     });
 }
