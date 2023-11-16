7359fc57d17698ee17afc9afdc7a95e4443e9ef6
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-11-16 18:01:03 +0600

    Update ProductList.vue

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -37,189 +37,16 @@
           </button>
         </div>
 
-        <div class="hidden sm:flex sm:gap-4">
-<!--          <div class="relative">-->
-<!--            <details class="group [&_summary::-webkit-details-marker]:hidden">-->
-<!--              <summary-->
-<!--                  class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"-->
-<!--              >-->
-<!--                <span class="text-sm font-medium"> Availability </span>-->
-
-<!--                <span class="transition group-open:-rotate-180">-->
-<!--                <svg-->
-<!--                    xmlns="http://www.w3.org/2000/svg"-->
-<!--                    fill="none"-->
-<!--                    viewBox="0 0 24 24"-->
-<!--                    stroke-width="1.5"-->
-<!--                    stroke="currentColor"-->
-<!--                    class="h-4 w-4"-->
-<!--                >-->
-<!--                  <path-->
-<!--                      stroke-linecap="round"-->
-<!--                      stroke-linejoin="round"-->
-<!--                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"-->
-<!--                  />-->
-<!--                </svg>-->
-<!--              </span>-->
-<!--              </summary>-->
-
-<!--              <div-->
-<!--                  class="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0"-->
-<!--              >-->
-<!--                <div class="w-96 rounded border border-gray-200 bg-white">-->
-<!--                  <header class="flex items-center justify-between p-4">-->
-<!--                    <span class="text-sm text-gray-700"> 0 Selected </span>-->
-
-<!--                    <button-->
-<!--                        type="button"-->
-<!--                        class="text-sm text-gray-900 underline underline-offset-4"-->
-<!--                    >-->
-<!--                      Reset-->
-<!--                    </button>-->
-<!--                  </header>-->
-
-<!--                  <ul class="space-y-1 border-t border-gray-200 p-4">-->
-<!--                    <li>-->
-<!--                      <label-->
-<!--                          for="FilterInStock"-->
-<!--                          class="inline-flex items-center gap-2"-->
-<!--                      >-->
-<!--                        <input-->
-<!--                            type="checkbox"-->
-<!--                            id="FilterInStock"-->
-<!--                            class="h-5 w-5 rounded border-gray-300"-->
-<!--                        />-->
-
-<!--                        <span class="text-sm font-medium text-gray-700">-->
-<!--                        In Stock (5+)-->
-<!--                      </span>-->
-<!--                      </label>-->
-<!--                    </li>-->
-
-<!--                    <li>-->
-<!--                      <label-->
-<!--                          for="FilterPreOrder"-->
-<!--                          class="inline-flex items-center gap-2"-->
-<!--                      >-->
-<!--                        <input-->
-<!--                            type="checkbox"-->
-<!--                            id="FilterPreOrder"-->
-<!--                            class="h-5 w-5 rounded border-gray-300"-->
-<!--                        />-->
-
-<!--                        <span class="text-sm font-medium text-gray-700">-->
-<!--                        Pre Order (3+)-->
-<!--                      </span>-->
-<!--                      </label>-->
-<!--                    </li>-->
-
-<!--                    <li>-->
-<!--                      <label-->
-<!--                          for="FilterOutOfStock"-->
-<!--                          class="inline-flex items-center gap-2"-->
-<!--                      >-->
-<!--                        <input-->
-<!--                            type="checkbox"-->
-<!--                            id="FilterOutOfStock"-->
-<!--                            class="h-5 w-5 rounded border-gray-300"-->
-<!--                        />-->
-
-<!--                        <span class="text-sm font-medium text-gray-700">-->
-<!--                        Out of Stock (10+)-->
-<!--                      </span>-->
-<!--                      </label>-->
-<!--                    </li>-->
-<!--                  </ul>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </details>-->
-<!--          </div>-->
-
-          <div class="relative">
-            <details class="group [&_summary::-webkit-details-marker]:hidden">
-              <summary
-                  class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"
-              >
-                <span class="text-sm font-medium"> Price </span>
-
-                <span class="transition group-open:-rotate-180">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-4 w-4"
-                >
-                  <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                  />
-                </svg>
-              </span>
-              </summary>
-
-              <div
-                  class="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0"
-              >
-                <div class="w-96 rounded border border-gray-200 bg-white">
-                  <header class="flex items-center justify-between p-4">
-                  <span class="text-sm text-gray-700">
-                    The highest price is $600
-                  </span>
-
-                    <button
-                        type="button"
-                        class="text-sm text-gray-900 underline underline-offset-4"
-                    >
-                      Reset
-                    </button>
-                  </header>
-
-                  <div class="border-t border-gray-200 p-4">
-                    <div class="flex justify-between gap-4">
-                      <label
-                          for="FilterPriceFrom"
-                          class="flex items-center gap-2"
-                      >
-                        <span class="text-sm text-gray-600">$</span>
-
-                        <input
-                            type="number"
-                            id="FilterPriceFrom"
-                            placeholder="From"
-                            class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                        />
-                      </label>
-
-                      <label for="FilterPriceTo" class="flex items-center gap-2">
-                        <span class="text-sm text-gray-600">$</span>
-
-                        <input
-                            type="number"
-                            id="FilterPriceTo"
-                            placeholder="To"
-                            class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                        />
-                      </label>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </details>
-          </div>
-        </div>
+        <div class="hidden sm:flex sm:gap-4"></div>
 
         <div class="hidden sm:block">
-          <label for="SortBy" class="sr-only">SortBy</label>
-
-          <select id="SortBy" class="h-10 rounded border-gray-300 text-sm">
-            <option>Sort By</option>
-            <option value="Title, DESC">Title, DESC</option>
-            <option value="Title, ASC">Title, ASC</option>
-            <option value="Price, DESC">Price, DESC</option>
-            <option value="Price, ASC">Price, ASC</option>
+          <label for="SortBy" class="sr-only">Sort By</label>
+          <select id="SortBy" class="h-10 rounded border-gray-300 text-sm" v-model="sortOption" @change="applySort">
+            <option value="">Sort By</option>
+            <option value="title,desc">Title, DESC</option>
+            <option value="title,asc">Title, ASC</option>
+            <option value="price,desc">Price, DESC</option>
+            <option value="price,asc">Price, ASC</option>
           </select>
         </div>
       </div>
@@ -260,6 +87,7 @@
 import { useCart } from "@/composables/useCart";
 import { onMounted, ref } from "vue";
 import { useProduct } from "@/composables/useProduct";
+import axios from "axios";
 
 const { handleFetchProducts } = useProduct();
 const { addToCart } = useCart();
@@ -271,6 +99,23 @@ onMounted(async () => {
   products.value = result.data;
 });
 
+const sortOption = ref('');
+
+async function applySort() {
+  if (!sortOption.value) return;
+
+  const [sortBy, order] = sortOption.value.split(',');
+
+  try {
+    const response = await axios.get('product/products', {
+      params: {sortBy, order}
+    });
+    // Assuming you have a reactive variable to store products
+    products.value = response.data;
+  } catch (error) {
+    console.error('Error fetching sorted products:', error);
+  }
+}
 // Add a product to the cart
 function handleAddToCart(product) {
   console.log(product)
