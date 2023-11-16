a82ddc97c1d90a2a82e09bdb43ccfe668d3eca8e
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-11-16 11:13:32 +0600

    get all products

--- src/api/products.js
+++ src/api/products.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import {API_URL} from "@/utils/consts";
+import axiosInstance from "@/utils/instance";
 
 export function getAllProducts() {
-    return axios.get(API_URL + 'product/all');
+    return axiosInstance.get(API_URL + 'product/all');
 }
 export function getProductByID(productId) {
     return axios.get(API_URL + `product/${productId}`);
