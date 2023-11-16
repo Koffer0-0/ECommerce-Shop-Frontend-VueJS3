2223e426ac66869f952924f20a2e3cd58c1df058
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-11-16 09:34:37 +0600

    Update products.js

--- src/api/products.js
+++ src/api/products.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {API_URL} from "@/utils/consts";
 
 export function getAllProducts() {
-    return axios.get(API_URL + 'products');
+    return axios.get(API_URL + 'product/all');
 }
 export function getProductByID(productId) {
     return axios.get(API_URL + `product/${productId}`);
