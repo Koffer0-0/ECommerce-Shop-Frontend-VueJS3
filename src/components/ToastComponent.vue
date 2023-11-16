3f35055ec5a40531c166fe33d117d392c7866963
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-11-16 17:44:24 +0600

    Create ToastComponent.vue

---
+++ src/components/ToastComponent.vue
@@ -0,0 +1,17 @@
+<!-- ToastNotification.vue -->
+<template>
+  <div v-if="toast.isVisible" class="fixed bottom-5 right-5 flex items-center space-x-2 bg-blue-500 text-white text-sm px-4 py-3 rounded shadow">
+    <span>{{ toast.message }}</span>
+    <button @click="closeToast">✕</button>
+  </div>
+</template>
+
+<script setup>
+import { inject } from 'vue';
+
+const toast = inject('toast');
+
+function closeToast() {
+  toast.isVisible.value = false;
+}
+</script>
