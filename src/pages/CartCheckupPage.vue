d6aaf206645ba7ee032f83895039b4ded8d29f12
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-11-16 14:37:11 +0600

    remove item from cart

--- src/pages/CartCheckupPage.vue
+++ src/pages/CartCheckupPage.vue
@@ -44,7 +44,8 @@
                   />
                 </form>
 
-                <button class="text-gray-600 transition hover:text-red-600">
+                <button class="text-gray-600 transition hover:text-red-600"
+                        @click="removeFromCart(item._id)">
                   <span class="sr-only">Remove item</span>
                   <svg
                       xmlns="http://www.w3.org/2000/svg"
