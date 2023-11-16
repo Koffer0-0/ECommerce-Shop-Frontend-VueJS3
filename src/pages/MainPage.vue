7013bcf4a4cfa63af5609b8e1ed060e05480fb02
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-11-16 13:44:17 +0600

    update banned content

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,39 +1,36 @@
 <template>
-  <section class="bg-gray-50">
-    <div
-        class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
-    >
-      <div class="mx-auto max-w-xl text-center">
-        <h1 class="text-3xl font-extrabold sm:text-5xl">
-          Understand User Flow.
-          <strong class="font-extrabold text-red-700 sm:block">
-            Increase Conversion.
-          </strong>
-        </h1>
+    <section class="bg-gray-50">
+      <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div class="mx-auto max-w-xl text-center">
+          <h1 class="text-3xl font-extrabold sm:text-5xl">
+            Elevate Your Style.
+            <strong class="font-extrabold text-teal-500 sm:block">
+              Discover Fashion Trends.
+            </strong>
+          </h1>
 
-        <p class="mt-4 sm:text-xl/relaxed">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-          tenetur fuga ducimus numquam ea!
-        </p>
+          <p class="mt-4 sm:text-xl text-gray-600">
+            Explore our exclusive collection where elegance meets everyday comfort. Delve into a wide range of styles from our latest arrivals that perfectly blend modern designs with timeless classics.
+          </p>
 
-        <div class="mt-8 flex flex-wrap justify-center gap-4">
-          <a
-              class="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-              href="/get-started"
-          >
-            Get Started
-          </a>
+          <div class="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+                class="block w-full rounded text-teal-400 px-12 py-3 text-sm font-medium text-white shadow hover:bg-teal-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                href="/products"
+            >
+              Shop Now
+            </a>
 
-          <a
-              class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-              href="/about"
-          >
-            Learn More
-          </a>
+<!--            <a-->
+<!--                class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"-->
+<!--                href="/about"-->
+<!--            >-->
+<!--              Our Story-->
+<!--            </a>-->
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   <recommendation-card></recommendation-card>
 </template>
 
