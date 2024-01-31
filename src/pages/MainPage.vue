ea1a8e69ed57baeafa43d72cee2b70ff867d0208
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2024-01-31 19:47:19 +0600

    Update MainPage.vue

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,43 +1,85 @@
 <template>
-    <section class="bg-gray-50">
-      <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-        <div class="mx-auto max-w-xl text-center">
-          <h1 class="text-3xl font-extrabold sm:text-5xl">
-            Elevate Your Style.
-            <strong class="font-extrabold text-teal-500 sm:block">
-              Discover Fashion Trends.
-            </strong>
-          </h1>
+  <section
+      class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat"
+  >
+    <div
+        class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
+    ></div>
 
-          <p class="mt-4 sm:text-xl text-gray-600">
-            Explore our exclusive collection where elegance meets everyday comfort. Delve into a wide range of styles from our latest arrivals that perfectly blend modern designs with timeless classics.
-          </p>
+    <div
+        class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+    >
+      <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+        <h1 class="text-3xl font-extrabold sm:text-5xl">
+          Let us find your
 
-          <div class="mt-8 flex flex-wrap justify-center gap-4">
-            <a
-                class="block w-full rounded text-teal-400 px-12 py-3 text-sm font-medium text-white shadow hover:bg-teal-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-                href="/products"
-            >
-              Shop Now
-            </a>
+          <strong class="block font-extrabold text-rose-700"> Forever Home. </strong>
+        </h1>
 
-<!--            <a-->
-<!--                class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"-->
-<!--                href="/about"-->
-<!--            >-->
-<!--              Our Story-->
-<!--            </a>-->
-          </div>
+        <p class="mt-4 max-w-lg sm:text-xl/relaxed">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+          numquam ea!
+        </p>
+
+        <div class="mt-8 flex flex-wrap gap-4 text-center">
+          <a
+              href="#"
+              class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+          >
+            Get Started
+          </a>
+
+          <a
+              href="#"
+              class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+          >
+            Learn More
+          </a>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
   <recommendation-card></recommendation-card>
+  <section class="bg-gray-50">
+    <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+      <div class="mx-auto max-w-xl text-center">
+        <h1 class="text-3xl font-extrabold sm:text-5xl">
+          Elevate Your Style.
+          <strong class="font-extrabold text-amber-600 sm:block">
+            Discover Fashion Trends.
+          </strong>
+        </h1>
+
+        <p class="mt-4 sm:text-xl text-gray-600">
+          Explore our exclusive collection where elegance meets everyday comfort. Delve into a wide range of styles from our latest arrivals that perfectly blend modern designs with timeless classics.
+        </p>
+
+        <div class="mt-8 flex flex-wrap justify-center gap-4">
+          <a
+              class="block w-full rounded px-12 py-3 text-sm font-medium shadow hover:bg-amber-500 focus:outline-none hover:text-gray-800 focus:ring active:orange-background-color sm:w-auto"
+              href="/products"
+          >
+            Shop Now
+          </a>
+
+          <!--            <a-->
+          <!--                class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"-->
+          <!--                href="/about"-->
+          <!--            >-->
+          <!--              Our Story-->
+          <!--            </a>-->
+        </div>
+      </div>
+    </div>
+  </section>
+  <ProductList></ProductList>
 </template>
 
 <script setup>
 import RecommendationCard from "@/components/RecommendationCard.vue";
 import {useProduct} from "@/composables/useProduct";
 import {onMounted} from "vue";
+import ProductList from "@/components/ProductList.vue";
 
 onMounted(() => {
 
